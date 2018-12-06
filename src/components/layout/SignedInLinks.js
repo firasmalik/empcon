62450a9032a9d6df86b575963881a043f30df0d7
firasmalik@gmail.com <firasmalik@gmail.com>
2018-12-06 12:42:16 +0530

    Adding redux

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -6,7 +6,7 @@ const SignedInLinks=()=>{
 
 		<ul className='right'>
 
-		<li><NavLink to='/'>New Project</NavLink></li>
+		<li><NavLink to='/create'>New Project</NavLink></li>
 		<li><NavLink to='/'>Logout</NavLink></li>
 		<li><NavLink to='/' className='btn btn-floating pink lighten-1'> Firas</NavLink></li>
 
