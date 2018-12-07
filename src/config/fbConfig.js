b7d129cd244cba939142a3694bb5b21dc3fa4e70
firasmalik@gmail.com <firasmalik@gmail.com>
2018-12-08 01:04:04 +0530

    input data ..firestore type error

---
+++ src/config/fbConfig.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+// Initialize Firebase
+var config = {
+   apiKey: "AIzaSyBZNTe3hZ_xVZ8JknsRjGV-EFcG8swaMg0",
+   authDomain: "empcon-6e43e.firebaseapp.com",
+   databaseURL: "https://empcon-6e43e.firebaseio.com",
+   projectId: "empcon-6e43e",
+   storageBucket: "empcon-6e43e.appspot.com",
+   messagingSenderId: "239192619837"
+ };
+ firebase.initializeApp(config);
+ firebase.firestore().settings({timestampsInSnapshots:true});
+
+ export default firebase;
